95b965476de3dfbbe6b0e373193e2b12d965d971
d.dolomanyuk <d.dolomanyuk@e100it.pl>
2023-11-15 10:06:22 +0100

    #11 Create Driver object

--- Drivers/Driver.cs
+++ Drivers/Driver.cs
@@ -1,8 +1,28 @@
-using System;
+using Microsoft.Playwright;
 
 namespace SpecFlowPlaywright.Drivers
 {
-    public class Driver
+    public class Driver : IDisposable
     {
+        private readonly Task<IPage> _page;
+        private IBrowser? _browser;
+        public Driver() => _page = InitializePlaywright();
+
+        public IPage Page => _page.Result;
+
+        private async Task<IPage> InitializePlaywright()
+        {
+            //Playwright
+            using var playwright = await Playwright.CreateAsync();
+            //Browser
+            _browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+            {
+                Headless = false
+            });
+            //Page
+            return await _browser.NewPageAsync();
+        }
+
+        public void Dispose() => _browser?.CloseAsync();
     }
 }
