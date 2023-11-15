d1ba478b282ce9a426670f0ac81b7fca78afd03d
d.dolomanyuk <d.dolomanyuk@e100it.pl>
2023-11-15 09:39:10 +0100

    init

---
+++ Drivers/Driver.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace SpecFlowPlaywright.Drivers
+{
+    public class Driver
+    {
+    }
+}
---
+++ Steps/EAAppTestSteps.cs
@@ -0,0 +1,38 @@
+﻿namespace SpecFlowPlaywright.Steps;
+
+[Binding]
+public sealed class EAAppTestSteps
+{
+    // For additional details on SpecFlow step definitions see https://go.specflow.org/doc-stepdef
+
+    private readonly ScenarioContext _scenarioContext;
+
+    public EAAppTestSteps(ScenarioContext scenarioContext)
+    {
+        _scenarioContext = scenarioContext;
+    }
+
+    [Given(@"I navigate to application")]
+    public void GivenINavigateToApplication()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"I click login link")]
+    public void GivenIClickLoginLink()
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Given(@"I enter following login details")]
+    public void GivenIEnterFollowingLoginDetails(Table table)
+    {
+        ScenarioContext.StepIsPending();
+    }
+
+    [Then(@"I see Employee List")]
+    public void ThenISeeEmployeeList()
+    {
+        ScenarioContext.StepIsPending();
+    }
+}
